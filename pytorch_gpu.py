fcca88289bb6db6de533bf8ac55bb2864cdeb646
Toner <tonerbp@cn-0-10-bilgin.engr.arizona.edu>
2022-12-08 14:19:02 -0700

    fixed

--- pytorch_gpu.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import torch
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-x = torch.zeros((3,3,3), device=device, dtype=torch.complex64)
-
-y = np.linspace(1,10,10)
-y = torch.tensor(y, device=device,dtype=torch.complex64)
